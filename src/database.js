ffd392d11ee7cd728a648c940ccbdc36b4c572ff
Thainá Pires <thainaspiress@gmail.com>
2023-04-30 15:03:37 -0300

    adding other routes

--- src/database.js
+++ src/database.js
@@ -19,7 +19,15 @@ export class Database {
     }
 
     select(table, search){
-        const data = this.#database[table] ?? []
+        let data = this.#database[table] ?? []
+        
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
 
         return data
     }
@@ -37,10 +45,37 @@ export class Database {
     }
 
     delete(table, id){
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
+        if (rowIndex > -1) {
+          this.#database[table].splice(rowIndex, 1)
+          this.#persist()
+        }
     }
 
     update(table, id, data){
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
+        if (rowIndex > -1) {
+          const old_data = this.#database[table][rowIndex]
+          const updated_at_data = {
+            updated_at: new Date()
+          }
+          this.#database[table][rowIndex] = { id, ...old_data, ...data, ...updated_at_data}
+          this.#persist()
+        }
+    }
+
+    complete(table, id){
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+          const old_data = this.#database[table][rowIndex]
+          const completed_at_data = {
+            completed_at: new Date()
+          }
+          this.#database[table][rowIndex] = { id, ...old_data, ...completed_at_data}
+          this.#persist()
+        }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from './database.js'
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
             const { title, description } = req.body
             const createdAt = new Date();
@@ -21,17 +22,54 @@ export const routes = [
             }
 
             database.insert('tasks', task)
-
             return res.writeHead(201).end()
         }
     },
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (req, res) => {
-            const tasks = database.select('tasks')
+            const { search } = req.query
+
+            const tasks = database.select('tasks', search ? {
+              title: search,
+              description: search
+            } : null)
 
             return res.end(JSON.stringify(tasks))
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('tasks', id)
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/tasks/:id'),
+        handler: (req, res) => {
+          const { id } = req.params
+          const { title, description } = req.body
+    
+          database.update('tasks', id, {
+            title,
+            description,
+          })
+    
+          return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'PATCH',
+        path: buildRoutePath('/tasks/:id/complete'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.complete('tasks', id)
+            return res.writeHead(204).end()
         }
     },
 ]
